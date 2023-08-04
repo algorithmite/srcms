7774b47ba85c5d4e02072ee2a4b4c95d5d1595c9
Luke Draper <lukepdraper@gmail.com>
2023-08-04 14:02:55 -0500

    Shuttle Setup

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, Route};
+use shuttle_poem::ShuttlePoem;
+
+#[handler]
+fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[shuttle_runtime::main]
+async fn poem() -> ShuttlePoem<impl poem::Endpoint> {
+    let app = Route::new().at("/", get(hello_world));
+
+    Ok(app.into())
 }
