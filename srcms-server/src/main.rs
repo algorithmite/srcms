24005377de957b6f8a6becf388f9b43fa566b279
Luke Draper <lukepdraper@gmail.com>
2023-08-22 22:30:50 -0500

    cargo fmt

--- srcms-server/src/main.rs
+++ srcms-server/src/main.rs
@@ -13,7 +13,8 @@ fn hello_world() -> &'static str {
 async fn poem(
     #[shuttle_shared_db::Postgres(
         local_uri = "postgres://postgres:{secrets.PASSWORD}@localhost:5433/postgres"
-    )] pool: PgPool,
+    )]
+    pool: PgPool,
     #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> ShuttlePoem<impl poem::Endpoint> {
