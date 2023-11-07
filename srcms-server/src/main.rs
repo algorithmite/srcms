96e5105b0bdf468d70e27c662f865b4a150e89bf
Luke Draper <lukepdraper@gmail.com>
2023-11-06 22:34:08 -0600

    Update to Shuttle 0.31.0

--- srcms-server/src/main.rs
+++ srcms-server/src/main.rs
@@ -1,8 +1,7 @@
-use poem::{get, handler, Route};
+use poem::{endpoint::StaticFilesEndpoint, get, handler, Route};
 use shuttle_poem::ShuttlePoem;
 use shuttle_secrets::SecretStore;
 use sqlx::PgPool;
-use std::path::PathBuf;
 
 #[handler]
 fn hello_world() -> &'static str {
@@ -14,10 +13,12 @@ async fn poem(
     #[shuttle_shared_db::Postgres(
         local_uri = "postgres://postgres:{secrets.PASSWORD}@localhost:5433/postgres"
     )]
-    pool: PgPool,
-    #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
-    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+    _pool: PgPool,
+    #[shuttle_secrets::Secrets] _secret_store: SecretStore,
 ) -> ShuttlePoem<impl poem::Endpoint> {
-    let app = Route::new().at("/", get(hello_world));
+    let app = Route::new().at("/", get(hello_world)).nest(
+        "/static",
+        StaticFilesEndpoint::new("./static").show_files_listing(),
+    );
     Ok(app.into())
 }
